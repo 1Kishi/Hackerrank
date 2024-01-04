dc4389ceffe15437e5e381bcaba5dc2f5e74cb2f
1Kishi <129410273+1Kishi@users.noreply.github.com>
2024-01-04 23:18:07 +0100

    Update max-of-4-integers.cpp
    https://www.hackerrank.com/challenges/c-tutorial-functions/problem?isFullScreen=true

--- max-of-4-integers.cpp
+++ max-of-4-integers.cpp
@@ -1,3 +1,23 @@
+/*
+https://www.hackerrank.com/challenges/c-tutorial-functions/problem?isFullScreen=true
+
+
+Write a function int max_of_four(int a, int b, int c, int d) which reads four arguments and returns the greatest of them.
+Print the greatest of the four integers. 
+PS: I/O will be automatically handled.
+
+Input Format
+Input will contain four integers -  , one in each line.
+Sample Input
+3
+4
+6
+5
+Sample Output
+6 
+*/
+
+
 #include <iostream>
 #include <cstdio>
 using namespace std;
